d1c2b22449d49c722ceb4796bd90b56b6e0f8b98
Haze <117860981+HansiFED@users.noreply.github.com>
2024-11-20 02:15:09 +0100

    Styling & Html:
    - Added a register page and styled it
    
    - Added a login page (not styled)
    
    - Added header icons for further use
    
    - Fix: Fixed a tailwind config and added a font family to both my one style sheet and the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./**/**.{html,js}", "!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inriaserif: ['"Inria Serif"', "serif"],
+        inriasans: ['"Inria Sans"', "serif"],
+      },
+    },
   },
   plugins: [],
 };
