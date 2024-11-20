03fcd349bd2876040de38ddaf41523fb1fcbb4c3
Haze <117860981+HansiFED@users.noreply.github.com>
2024-11-20 02:18:16 +0100

    JS:
    - Added constants that will make the API more manage-able
    
    - Added useful mjs files that I will be using moving forward

---
+++ API/constants.mjs
@@ -0,0 +1,11 @@
+export const API_KEY = "1fa70db6-66b3-487c-834e-bcd2d13da983";
+
+export const accessToken = localStorage.getItem("accessToken");
+
+export const API_BASE = "https://v2.api.noroff.dev";
+
+export const API_AUTH = `${API_BASE}/auth`;
+
+export const API_AUTH_LOGIN = `${API_AUTH}/login`;
+
+export const API_AUTH_REGISTER = `${API_AUTH}/register`;
