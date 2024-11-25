3a9fc8de2858f886fb209c737be5f5152585021d
Haze <117860981+HansiFED@users.noreply.github.com>
2024-11-25 16:27:46 +0100

    Added auth guard function that redirects user if not logged in

---
+++ Utilities/authguard.mjs
@@ -0,0 +1,8 @@
+import { accessToken } from "../API/constants.mjs";
+
+export function authCheck() {
+  if (!accessToken) {
+    window.alert("You must be signed in to view this page");
+    window.location.href = "/";
+  }
+}
